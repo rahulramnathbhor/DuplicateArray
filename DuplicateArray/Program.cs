19767b2ec9e372c6a41baf2c4ae5ad959484f8ab
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 11:38:14 +0530

    Add project files.

---
+++ DuplicateArray/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateArray
+{
+    class GFG
+    {
+
+
+
+        static void printRepeating(int[] arr, int size)
+
+        {
+
+            int i, j;
+
+
+
+            Console.Write("Duplicate Numbers are :");
+
+            for (i = 0; i < size - 1; i++)
+
+            {
+
+                for (j = i + 1; j < size; j++)
+
+                {
+
+                    if (arr[i] == arr[j])
+
+                        Console.WriteLine(arr[i] + " ");
+
+                }
+
+            }
+
+        }
+
+        
+
+        public static void Main()
+
+        {
+
+            int[] arr = {1,2,3,2,4,3,5 };
+
+            int arr_size = arr.Length;
+
+
+
+            printRepeating(arr, arr_size);
+
+        }
+    }
+}
+
+
+
+
+
+    
+
+       
+        
+    
+
